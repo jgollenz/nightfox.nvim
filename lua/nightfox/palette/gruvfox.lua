7b6c6fc2a9fa75104eb76a910b1729ecd23d3bee
Jonas Gollenz <jgollenz@outlook.com>
2022-05-07 15:38:40 +0200

    foo

--- lua/nightfox/palette/gruvfox.lua
+++ lua/nightfox/palette/gruvfox.lua
@@ -6,23 +6,22 @@ local meta = {
   light = false,
 }
 
--- TODO add the shades
+-- TODO black? white?
 
 -- stylua: ignore
 local palette = {
-  black   = Shade.new("#282828", "#47407d", "#322e42"),
-  red     = Shade.new("#cc241d", "#f083a2", "#d84f76"),
-  green   = Shade.new("#98971a", "#b1d196", "#8aa872"),
-  yellow  = Shade.new("#d79921", "#f9cb8c", "#e6a852"),
-  blue    = Shade.new("#458588", "#65b1cd", "#4a869c"),
-  magenta = Shade.new("#b16286", "#ccb1ed", "#a580d2"),
-  cyan    = Shade.new("#689d6a", "#a6dae3", "#7bb8c1"),
-  white   = Shade.new("#ebdbb2", "#e2e0f7", "#b1acde"),
-  orange  = Shade.new("#d65d0e", "#f0a4a2", "#d6746f"),
-  pink    = Shade.new("#eb98c3", "#f0a6cc", "#d871a6"),
+  black   = Shade.new("#282828", "#1d2021", "#32302f"),
+  red     = Shade.new("#cc241d", "#fb4934", "#9d0006"),
+  green   = Shade.new("#98971a", "#b8bb26", "#79740e"),
+  yellow  = Shade.new("#d79921", "#fabd2f", "#b57614"),
+  blue    = Shade.new("#458588", "#83a598", "#076678"),
+  magenta = Shade.new("#b16286", "#d3869b", "#8f3f71"),
+  cyan    = Shade.new("#689d6a", "#8ec07c", "#427b58"),
+  white   = Shade.new("#fbf1c7", "#f9f5d7", "#f2e5bc"),
+  orange  = Shade.new("#d65d0e", "#fe8019", "#af3a03"),
+  pink    = Shade.new("#b16286", "#d3869b", "#8f3f71"), -- TODO I just copied this from magenta
 
-  -- TODO
-  comment = "#817c9c",
+  comment = "#928374",
 
   bg0     = "#282828", -- Dark bg (status line and float)
   bg1     = "#3c3836", -- Default bg
@@ -36,57 +35,58 @@ local palette = {
   fg3     = "#bdae93", -- Darker fg (line numbers, fold colums)
 
   -- TODO
-  sel0    = "#433c59", -- Popup bg, visual selection bg
-  sel1    = "#63577d", -- Popup sel bg, search bg
+  sel0    = "#665c54",      -- Popup bg, visual selection bg
+  sel1    = "#a89984",      -- Popup sel bg, search bg
 }
 
 local function generate_spec(pal)
   -- stylua: ignore start
   local spec = {
     bg0  = pal.bg0,  -- Dark bg (status line and float)
-    bg1  = pal.bg1,  -- Default bg
-    bg2  = pal.bg2,  -- Lighter bg (colorcolm folds)
-    bg3  = pal.bg3,  -- Lighter bg (cursor line)
-    bg4  = pal.bg4,  -- Conceal, border fg
+    bg1  = pal.bg0,  -- Default bg
+    bg2  = pal.pink.base,  -- Lighter bg (colorcolm folds)
+    bg3  = pal.pink.base,  -- Lighter bg (cursor line)
+    bg4  = pal.pink.base,  -- Conceal, border fg
 
     fg0  = pal.fg0,  -- Lighter fg
     fg1  = pal.fg1,  -- Default fg
     fg2  = pal.fg2,  -- Darker fg (status line)
-    fg3  = pal.fg3,  -- Darker fg (line numbers, fold colums)
+    fg3  = pal.bg4,         -- Darker fg (line numbers, fold colums)
 
     sel0 = pal.sel0, -- Popup bg, visual selection bg
     sel1 = pal.sel1, -- Popup sel bg, search bg
   }
 
   spec.syntax = {
-    bracket     = spec.fg2,           -- Brackets and Punctuation
-    builtin0    = pal.red.base,       -- Builtin variable
+    bracket     = spec.fg1,           -- Brackets and Punctuation
+    builtin0    = pal.orange.bright,       -- Builtin variable
     builtin1    = pal.cyan.bright,    -- Builtin type
     builtin2    = pal.orange.bright,  -- Builtin const
     builtin3    = pal.red.bright,     -- Not used
     comment     = pal.comment,        -- Comment
-    conditional = pal.magenta.bright, -- Conditional and loop
+    conditional = pal.red.bright,           -- Conditional and loop
     const       = pal.orange.bright,  -- Constants, imports and booleans
     dep         = spec.fg3,           -- Deprecated
-    field       = pal.blue.base,      -- Field
-    func        = pal.blue.bright,    -- Functions and Titles
-    ident       = pal.cyan.base,      -- Identifiers
-    keyword     = pal.magenta.base,   -- Keywords
-    number      = pal.orange.base,    -- Numbers
-    operator    = spec.fg2,           -- Operators
-    preproc     = pal.pink.bright,    -- PreProc
+    field       = pal.blue.bright,          -- Field
+    func        = pal.green.bright,         -- Functions and Titles
+    ident       = pal.cyan.bright,      -- Identifiers
+    keyword     = pal.red.bright,           -- Keywords
+    number      = pal.magenta.bright,       -- Numbers
+    operator    = spec.fg1,                 -- Operators
+    preproc     = pal.cyan.bright,          -- PreProc
     regex       = pal.yellow.bright,  -- Regex
     statement   = pal.magenta.base,   -- Statements
-    string      = pal.green.base,     -- Strings
-    type        = pal.yellow.base,    -- Types
-    variable    = pal.white.base,     -- Variables
+    string      = pal.green.bright,         -- Strings
+    type        = pal.yellow.bright,        -- Types
+    -- TODO this is a bit off, compared to ellisonleao version
+    variable    = pal.fg0,            -- Variables
   }
 
   spec.diag = {
-    error = pal.red.base,
-    warn  = pal.yellow.base,
-    info  = pal.blue.base,
-    hint  = pal.green.base,
+    error = pal.red.bright,
+    warn  = pal.yellow.bright,
+    info  = pal.blue.bright,
+    hint  = pal.green.bright,
   }
 
   -- TODO
@@ -106,10 +106,10 @@ local function generate_spec(pal)
   }
 
   spec.git = {
-    add      = pal.green.base,
-    removed  = pal.red.base,
-    changed  = pal.yellow.base,
-    conflict = pal.orange.base,
+    add      = pal.green.bright,
+    removed  = pal.red.bright,
+    changed  = pal.yellow.bright,
+    conflict = pal.orange.bright,
     ignored  = pal.comment,
   }
 
